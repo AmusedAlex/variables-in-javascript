2f3b28846c0b7a308f1aea59fb1d9e693e3b6370
AmusedAlex <114648306+AmusedAlex@users.noreply.github.com>
2022-10-04 15:40:17 +0200

    EXTRA Homework Pasta Carbonara

---
+++ index_carbonara.js
@@ -0,0 +1,45 @@
+// Ingredients:
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolks = 6;
+let percorinoRomano = 50;
+let blackPepper = 4;
+
+// First we cut the guanciale into 1cm layers, then into long strips.
+
+// Combining egg yolks with finely grated Percorino Romano
+let combinedEggYolksAndPercorinoRomano = eggYolks + percorinoRomano;
+console.log("Weight of eggs and cheese is", combinedEggYolksAndPercorinoRomano);
+
+// Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount (1) of it to the yolks and chees mixture
+let onePepper = blackPepper / 4;
+let threePepper = (blackPepper / 4) * 3;
+
+let combinedEggCheeseOnePepper = combinedEggYolksAndPercorinoRomano + onePepper;
+console.log("Weight of eggs, cheese and pepper is", combinedEggCheeseOnePepper);
+// we have 3g left of pepper
+
+/*Browning the strips of guanciale for 3 minutes at medium heat, then 1 minune on high heat til crisp, 
+then turning off the heat, taking out the guanciale, leaving only the grease on the pan cool down.*/
+
+/* Pouring 2 spoons of the reserved hot water into the part 
+of the frying pan containing the cooled guanciale and turning on the heat: */
+
+/* Turning off the heat and setting aside for 1 minute, then transfering the pasta to the same pan and 
+mixing them together. */
+
+let combinedGuancialePasta = guanciale + spaghetti;
+console.log(
+  "Weight of combined Guanciale and Spaghetti is",
+  combinedGuancialePasta
+);
+
+// Adding the yolk and cheese mixture rapidly
+
+let combinedPastaSauce = combinedEggCheeseOnePepper + combinedGuancialePasta;
+console.log("Weight of Pasta including Sauce is", combinedPastaSauce);
+
+// Adding the remaining roasted black pepper (3)
+
+let pepperedPastaSauce = combinedPastaSauce + threePepper;
+console.log("Total weight of the meal is", pepperedPastaSauce);
