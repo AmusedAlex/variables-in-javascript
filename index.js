f8d981f1196f0de6c79c7015df7f532c4a9d9949
AmusedAlex <alex.spomer74@gmail.com>
2022-10-04 14:55:23 +0200

    added index.js to index.html via <script>

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Alexander";
+let surName = "Spomer";
+
+console.log("hello, my name is", firstName, " ", surName, ".");
+
+let age = 30;
+let currentYear = 2022;
+
+let birthYear = currentYear - age;
+
+console.log("My birth year is ", birthYear);
